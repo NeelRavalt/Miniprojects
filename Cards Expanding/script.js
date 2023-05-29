5321f046169419891748755a5de403b38f800206
Neel <neelraval16113@gmail.com>
2023-05-29 16:15:03 +0530

    card slider

---
+++ Cards Expanding/script.js
@@ -0,0 +1,18 @@
+const panels =
+    document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        // class "active" to the clicked panel
+        panel.classList.add('active');
+    });
+});
+
+// remove the class "active" from all the panels
+function removeActiveClasses() {
+       
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    });
+}
