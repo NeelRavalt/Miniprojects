0f2c660823796d21be400517c2ac7bea4a2a8f5c
Neel <neelraval16113@gmail.com>
2023-06-17 10:51:18 +0530

    commit

--- Cards Expanding/script.js
+++ Cards Expanding/script.js
@@ -13,6 +13,6 @@ panels.forEach((panel) => {
 function removeActiveClasses() {
        
     panels.forEach((panel) => {
-        panel.classList.remove('active');
+        panel.classList.remove('active'); 
     });
 }
